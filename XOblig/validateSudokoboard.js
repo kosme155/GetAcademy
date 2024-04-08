28f6536ebaf5a9a2c0ae9757c8de432bc6e13718
Ruben <anda94@live.no>
2024-04-08 22:43:19 +0200

    rt5

--- XOblig/validateSudokoboard.js
+++ XOblig/validateSudokoboard.js
@@ -1,3 +1,16 @@
-function validateSudokuboard(sudoboardString){
+function validateSudokuboard(sudoboardString) {
+    // Sjekk om board lengdt ikke er 16
+    if (sudoboardString.length !==16) {
+        return 'ugyldig brett, feil lengde';
+    }
+    //Sjekker om board har input utenom 1-4 eller blank.
+    const validChars = ['1', '2' ,'3' , '4', ''];
+    for (let i = 0 ; i < sudoboardString.length; i++) {
+        if (!validChars.includes(sudoboardString[i])) {
+            return 'ugyldig brett, ugyldig tegn';
+        }
+    }
+
+
     return 'helt utfylt, ingen feil';
 }
